84cb39a0d8ed9dd9c95ca43122eb258dc4734242
Your Name <you@example.com>
2021-06-02 00:24:19 +0300

    first commit

---
+++ resources/views/welcome2.blade.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Koenigsegg</title>
+    <link rel="stylesheet" href="main.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+</head>
+<body>
+  
+    <header>
+        <button type="button" class="btn btn-link" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Sign in
+          </button>
+
+          <script>
+              let x =0
+        function alerted(){
+            alert('Вы не прошли авторизацию')
+            let abc = document.querySelector('.modal-title')
+            abc.textContent = 'Авторизация' + x++
+        }
+          </script>
+          
+    
+
+          </script>
+          <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Авторизация</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <form action="">
+                      <div class="form-row mb-3">
+                          <label for = inputEmail "" class="col-sm-2 col-form-label">Email</label>
+                          <div class="col-sm-10"> 
+                              <input type="email" class="form-control" id="inputEmail" required>
+                          </div>
+                      </div>
+                      <div class="form-row mb-3">
+                        <label for = inputPass "" class="col-sm-2 col-form-label">Password</label>
+                        <div class="col-sm-10"> 
+                            <input type="password" class="form-control" id="inputPass" required >
+
+                        </div>
+                    </div>
+                  </form>
+                
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"  onclick=" alerted()">Войти</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+        <h1>
+            <img src="https://www.koenigsegg.com/wp-content/uploads/koenigsegg_vert_black.svg"/>
+        </h1>
+    </header>
+    <video class="_autoplay_vid" id="autoplayVideo" autoplay muted playsinline>
+        <source src="https://www.koenigsegg.com/wp-content/themes/koenigsegg/gemera/assets/Gemera_Hotspot_anim_1080p.mp4">
+    </video>
+
+    <div class="test4">
+        <h5>Koenigsegg — Самые Быстрые Серийные Автомобили</h5>
+    </div>
+
+    <div class="test">
+        <aside>
+            <img class="photo4" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/2016-03-01_Geneva_Motor_Show_1333.JPG/1280px-2016-03-01_Geneva_Motor_Show_1333.JPG" />
+        </aside>
+
+        <main>
+            <h2>Koenigsegg Regera — автомобиль, выпущенный в 2017 году шведской компанией Koenigsegg под контролем Кристиана фон Кёнигсегга.</h2>
+            <p>
+                Максимальная скорость — 410 км/ч, разгон до 100 км/ч длится 2,8 секунды, вес автомобиля — 1590 кг. Гиперкар оснащен 5-литровым твин-турбо «V8», который развивает 1100 лошадиных сил и 1280 Нм крутящего момента при 4100 оборотах в минуту (в диапазоне 2700-6170 оборотов момент достигает 1000 Нм).
+            </p>
+            <p>
+                В силовую установку также входят три электромотора, разработанные фирмой YASA. Два из них расположены по одному в задних колесах (по 245 сил и 260 Нм каждый) и один на валу от ДВС (217 сил, 300 Нм). Для модели была разработана новая коробка передач, которая носит название Koenigsegg Direct Drive Transmission (KDD).
+            </p>
+            <p>
+                Питает электромотор комплект литиевых батарей. Ёмкость батарей — 9,27 кВт*ч, объем 67 л, а масса — 115 кг. На электротяге гиперкар может проехать от 35 до 50 км. Батареи заряжаются с помощью системы рекуперации энергии при торможении, или от розетки.
+            </p>
+            <p>
+                Автомобиль имеет задний привод, что немного ухудшает его управляемость по сравнению, например, с такими машинами, как Veyron или Chiron, но в то же время сильно снижает вес.
+            </p>
+        </main>
+    </div>
+
+    <div class="test 2">
+        <aside>
+            <img class="photo3" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Koenigsegg_Agera_RS.JPG/1280px-Koenigsegg_Agera_RS.JPG" />
+        </aside>
+    
+
+        <main>
+        <h3>
+            Koenigsegg Agera RS — гиперкар производства компании Koenigsegg. Эта модификация легче и мощнее базовой Agera. Agera RS был представлен общественности в 2015 году на выставке автомобилей в Женеве. Начальная стоимость автомобиля на данный момент примерно составляет 2 млн долларов США.</h3>
+            <p>
+                Agera RS оснащена алюминиевой пятилитровой V-образной восьмеркой с твин-турбонаддувом, который даёт 1,3 бара давлением наддува; четыре клапана на цилиндр (всего 32 клапана). Доступен вариант двигателя 1 МВт (1341 лошадиная сила).
+            </p>
+            <p>
+                Koenigsegg и Bugatti являются вечными соперниками в скоростях и на рынке. В 2017 году Bugatti выпускают свой новый автомобиль — Bugatti Chiron, который был заявлен якобы самым быстрым автомобилем в мире[4]. Также Bugatti назвали свои 41,96 секунды мировым рекордом[5], но спустя месяц Koenigsegg решили замерить свой Agera RS в упражнении 0-400-0 км/час, как и Bugatti замеряли свой Chiron. В первом заезде[6] Agera RS выполнила это упражнение за 36,44 секунды, тем самым уже сокрушив рекорд Bugatti. Затем, через месяц Koenigsegg вновь ставят новые рекорды, а именно: «самый быстрый серийный автомобиль» и «самый быстрый на общественном шоссе», к тому же, Agera RS удалось побить свой собственный рекорд в упражнении 0-400-0 км/час, поставив новый рекорд в 33,87 секунды.
+            </p>
+        </main>
+    </div>
+
+    <div class="test 3">
+        <aside>
+            <img class="photo2" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/GIMS_2019%2C_Le_Grand-Saconnex_%28GIMS0833%29.jpg/1280px-GIMS_2019%2C_Le_Grand-Saconnex_%28GIMS0833%29.jpg" />
+        </aside>
+
+        <main>
+            <h4>Мегакар Koenigsegg Jesko: 1600 «лошадей» и шесть сцеплений</h4>
+            <p>
+                Во-первых, пятилитровый битурбомотор V8 обзавелся новым «плоским» коленвалом — из-за этого пассивные опоры двигателя пришлось заменить активными. А два турбонагнетателя дополнены 20-литровым углепластиковым воздушным ресивером и небольшим электрическим компрессором — такая система позволяет почти полностью исключить турбозадержки. На 95-м бензине мотор выдает 1280 л.с., однако при использовании биотоплива Е85 отдача вырастает до 1600 л.с. и 1500 Нм! Причем две трети максимального момента доступны в диапазоне от 2700 до 6170 об/мин.
+            </p>
+            <p>
+                Кузов мегакара Koenigsegg Jesko снабжен массой гидроцилиндров: они помогают открывать двери, передний и задний обтекатели, но главное — управлять активной аэродинамикой и избегать приседаний на заднюю ось при разгоне. Кстати, при скорости 250 км/ч на суперкар действует прижимная сила 800 кг, при 275 км/ч — одна тонна, а максимальное значение 1,4 т достигается при максимальной скорости, которую производитель для версии с развитым обвесом и антикрылом не сообщает. Однако модификация с упрощенной аэродинамикой, по расчетам, сможет разгоняться как минимум до 482 км/ч.
+            </p>
+        </main>
+    </div>
+
+    <div class="World__records">
+        Мировой Рекорд
+    </div>
+
+    <div class="world__record">
+        <h6>Koenigsegg Agera RS</h6>
+        <img class="photo" src="https://autoreview.ru/images/gallery/%D0%9D%D0%BE%D0%B2%D0%BE%D1%81%D1%82%D0%B8/2017/November/07/koenigsegg-agera-rs2.jpg" />
+        <p>
+                Официальные мировые рекорды
+        </p>
+        <p>
+            На момент 2017 года Agera RS уже успел поставить три официальных мировых рекорда:
+        </p>
+        <p>
+            Самый быстрый серийный автомобиль: 445 км/час
+        </p>
+        <p>
+            Самый быстрый из 0-400-0 км/час: 33,87 секунды
+        </p>
+        <p>
+            Самый быстрый на общественном шоссе: 456 км/час
+        </p>
+    </div>
+
+  
+    <table>
+        <tr>
+            <th>Koenigsegg Regera</th>
+            <th>Koenigsegg Agera RS</th>
+            <th>Мегакар Koenigsegg Jesko</th>
+        </tr>
+        <tr>
+            <td>Regera использует 5-литровый турбированный V8 Koenigsegg вместе с тремя электромоторами. Они питаются от 620-вольтного батарейного блока емкостью 9 кВтч с водяным охлаждением. По словам Кенигсегга, этот блок обладает самой высокой плотностью энерговыделения, и равных ему в автоиндустрии нет.</td>
+            <td>Agera RS оснащена алюминиевой пятилитровой V-образной восьмеркой с твин-турбонаддувом, который даёт 1,3 бара давлением наддува; четыре клапана на цилиндр (всего 32 клапана). Доступен вариант двигателя 1 МВт (1341 лошадиная сила).</td>
+            <td>Пятилитровый битурбомотор V8 обзавелся новым «плоским» коленвалом — из-за этого пассивные опоры двигателя пришлось заменить активными. А два турбонагнетателя дополнены 20-литровым углепластиковым воздушным ресивером и небольшим электрическим компрессором — такая система позволяет почти полностью исключить турбозадержки.</td>
+        </tr>
+    </table>
+
+        <section class="gallery">
+            <div class="container">
+
+                <div class="row0">
+                   <span>Мегакар Koenigsegg Jesko</span>
+                   <span>Мегакар Koenigsegg Jesko</span>
+                </div>
+                <div class="row1">
+                    <div><img src="https://www.farom.tv/wp-content/uploads/2019/08/001.jpg" alt></div>
+                   <div><img src="https://www.farom.tv/wp-content/uploads/2019/08/001.jpg" alt></div>
+                </div>
+                <div class="row0">
+                    <span>Мегакар Koenigsegg Jesko</span>
+                    <span>Мегакар Koenigsegg Jesko</span>
+                    <span>Мегакар Koenigsegg Jesko</span>
+                 </div>
+                <div class="row2">
+                    <div><img src="https://www.farom.tv/wp-content/uploads/2019/08/001.jpg" alt></div>
+                    <div><img src="https://www.farom.tv/wp-content/uploads/2019/08/001.jpg" alt></div>
+                    <div><img src="https://www.farom.tv/wp-content/uploads/2019/08/001.jpg" alt></div>
+                </div>
+                <div class="row0">
+                    <span>Мегакар Koenigsegg Jesko</span>
+                    <span>Мегакар Koenigsegg Jesko</span>
+                    <span>Мегакар Koenigsegg Jesko</span>
+                    <span>Мегакар Koenigsegg Jesko</span>
+                 </div>
+                <div class="row3">
+                    <div><img src="https://www.farom.tv/wp-content/uploads/2019/08/001.jpg" alt></div>
+		            <div><img src="https://www.farom.tv/wp-content/uploads/2019/08/001.jpg" alt></div>
+		            <div><img src="https://www.farom.tv/wp-content/uploads/2019/08/001.jpg" alt></div>
+                    <div><img src="https://www.farom.tv/wp-content/uploads/2019/08/001.jpg" alt></div>
+                </div>
+                <div class="row0">
+                    <span>Мегакар Koenigsegg Jesko</span>
+                    <span>Мегакар Koenigsegg Jesko</span>
+                    <span>Мегакар Koenigsegg Jesko</span>
+                    <span>Мегакар Koenigsegg Jesko</span>
+                    <span>Мегакар Koenigsegg Jesko</span>
+                 </div>
+                <div class="row4">
+                    <div><img src="https://www.farom.tv/wp-content/uploads/2019/08/001.jpg" alt></div>
+                    <div><img src="https://www.farom.tv/wp-content/uploads/2019/08/001.jpg" alt></div>
+		            <div><img src="https://www.farom.tv/wp-content/uploads/2019/08/001.jpg" alt></div>
+                    <div><img src="https://www.farom.tv/wp-content/uploads/2019/08/001.jpg" alt></div>
+	                <div><img src="https://www.farom.tv/wp-content/uploads/2019/08/001.jpg" alt></div>
+                    
+                </div>
+            </div>
+        </gallery>
+
+    
+    <div id="contacts">
+        <center><h5>Обратная связь</h5></center>
+        <form id="form_input">
+            <label for="Name">Имя<span>*</span></label><br>
+            <input type="text" placeholder="Введите имя" name="name" id="name"><br>
+            <label for="email">Ваша почта<span>*</span></label><br>
+            <input type="email" placeholder="Введите email" name="email" id="email">
+            <label for="message">Сообщение<span>*</span></label><br>
+            <textarea placeholder="Введите ваше сообщение" name="message" id="message"></textarea>  
+            <button type="button" class="btn btn-warning">Отправить</button>
+        </form>
+    </div>
+    
+
+        <img src="https://www.koenigsegg.com/wp-content/themes/koenigsegg/gemera/assets/images/svg/Koenigsegg_footer.svg">
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+</body>
+</html>
+     
+
+
